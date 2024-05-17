599508691749f7410e207f99b7daa4be42c9ad19
Andrii_notebok_mini <laggerfeedlag1992@gmail.com>
2024-05-18 00:42:01 +0300

    rename file and url

---
+++ WebApplication4/WebApplication4/Controllers/AzureController.cs
@@ -0,0 +1,22 @@
+﻿using AzureSearch.Quickstart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AzureController : ControllerBase
+    {
+        private ConnectAzure connect = new();
+        [HttpPost(Name = "GetAzure")]
+        public List<Files> Get([FromBody] string model)
+        {
+            List<Files> list = null;
+            if (connect != null)
+            {
+                list = connect.ConnectSearchFiles(model);
+            }
+            return list;
+        }
+    }
+}
