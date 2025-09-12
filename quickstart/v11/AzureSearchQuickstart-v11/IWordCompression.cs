7120d4f2ef2cbf8878dd91bad614f6408b9ca4e9
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-12 18:31:38 +0300

    first commit. Added interface

---
+++ quickstart/v11/AzureSearchQuickstart-v11/IWordCompression.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSearchQuickstart_v11
+{
+    interface IWordCompression
+    {
+        void Compression(string _Text);
+    }
+}
