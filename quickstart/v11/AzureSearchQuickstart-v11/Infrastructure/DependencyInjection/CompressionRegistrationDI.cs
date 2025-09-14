4d95258731567ab246723b002021fd85d0528c8f
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-14 21:43:53 +0300

    added folder for DI

---
+++ quickstart/v11/AzureSearchQuickstart-v11/Infrastructure/DependencyInjection/CompressionRegistrationDI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AzureSearchQuickstart_v11.Services.Compression;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AzureSearchQuickstart_v11.Infrastructure.DependencyInjection
+{
+    public class CompressionRegistrationDI
+    {
+        private readonly ServiceProvider provider;
+
+        private static readonly Lazy<CompressionRegistrationDI> _instance = new(()=> new CompressionRegistrationDI());
+
+        public static CompressionRegistrationDI Instance => _instance.Value;
+
+
+        private CompressionRegistrationDI()
+        {
+            var services = new ServiceCollection();
+            services.AddKeyedTransient<IWordCompression, PopularWords>("PopularWords");
+            services.AddKeyedTransient<IWordCompression, Services.Compression.Rake>("Rake");
+            services.AddKeyedTransient<IWordCompression, CharacterIndexing>("CharacterIndexing");
+
+            provider = services.BuildServiceProvider();
+        }
+
+
+        public IWordCompression TryGet(string Key) => provider.GetRequiredKeyedService<IWordCompression>(Key);
+
+    }
+}
---
+++ quickstart/v11/AzureSearchQuickstart-v11/Infrastructure/DependencyInjection/GetTextFromFileDI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AzureSearchQuickstart_v11.Services.Text.ReadText;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AzureSearchQuickstart_v11.Infrastructure.DependencyInjection
+{
+    class GetTextFromFileDI
+    {
+        private IServiceProvider serviceProvider;
+        private static readonly Lazy<GetTextFromFileDI> getTextFromFile = new(()=> new GetTextFromFileDI());
+
+        public static GetTextFromFileDI Instance => getTextFromFile.Value;
+        private GetTextFromFileDI()
+        {
+            var services = new ServiceCollection();
+            services.AddKeyedSingleton<IReadFile, ReadTxt> (".txt");
+            services.AddKeyedTransient<IReadFile, ReadPdf>(".pdf");
+            services.AddKeyedTransient<IReadFile, ReadDocx>(".docx");
+            services.AddKeyedTransient<IReadFile, ReadDoc>(".doc");
+
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        public IReadFile GetRead(string Key) => serviceProvider.GetRequiredKeyedService<IReadFile>(Key);
+    }
+}
