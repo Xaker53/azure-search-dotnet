f6b584dd08103388ca10424124f8ba174283a4f5
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-19 01:17:24 +0300

    Use cases

---
+++ WebApplication4/Application/UserReadService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interface;
+using Core.Interfaces;
+using Core.Models;
+
+namespace Application
+{
+    public class UserReadService: IUserGetInfoService
+    {
+        private readonly IRead GetByIntfo;
+
+        public UserReadService(IRead post)
+        {
+            this.GetByIntfo = post;
+        }
+
+        public async Task<User> GetInfoGmail(string Gmail)
+        {
+            return await GetByIntfo.GetByGmail(Gmail);
+        }
+    }
+}
---
+++ WebApplication4/Application/UserService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Core.Models;
+using Core.Interfaces;
+
+
+namespace UserService
+{
+    public static class UserService
+    {
+        //private ICreate create;
+
+        //public UserService (ICreate create)
+        //{
+        //    this.create = create;
+        //}
+
+        //public async Task Add (User user)
+        //{
+        //    await create.Add (user);
+        //}
+
+    }
+
+}
+
+
