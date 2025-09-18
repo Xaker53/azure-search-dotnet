7c97a961fe4b2f51d523c837113f15e2dfb43f46
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-19 01:24:33 +0300

    added unitTest for request in DB

--- quickstart/v11/Test Algorithm/TestDB.cs
+++ quickstart/v11/Test Algorithm/TestDB.cs
@@ -2,28 +2,91 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Azure;
 using CodeFirst.Models;
+using Core.Models;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Newtonsoft.Json;
 
 namespace Test_Algorithm
 {
     public class TestDB
     {
-        [Fact]
-        public void TestValidatorDb()
+        private HttpClient mClient = new()
         {
-            var User = new User();
-            User.UserName = "test";
-            User.Password = "test";
-            User.UserGmail = "testf@gmail.com";
+            BaseAddress = new Uri("http://127.0.0.1:5191/api/azure")
+        };
+        //[Fact]
+        //public void TestValidatorDb()
+        //{
+        //    var User = new User();
+        //    User.UserName = "test";
+        //    User.Password = "test";
+        //    User.UserGmail = "testf@gmail.com";
 
-            UserdbContext dbContext = new UserdbContext();
-            Validator.ValidateObject(User, new ValidationContext(User), true);
-            dbContext.Add(User);
-            dbContext.SaveChanges();
+        //    UserdbContext dbContext = new UserdbContext();
+        //    Validator.ValidateObject(User, new ValidationContext(User), true);
+        //    dbContext.Add(User);
+        //    dbContext.SaveChanges();
+        //}
+
+        [Fact]
+
+        public async void TestAddDb()
+        {
+            var user = new Core.Models.User
+            {
+                UserName = "test",
+                UserGmail = "test1@gmail.com",
+                Password = "123",
+                IndexName = "myIndex",
+                ApiKey = "abc123"
+            };
+
+            using var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync("https://localhost:7156/api/Create", Jsonconver(user));
+            TryCatch(response);
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+
+
+        }
+        [Fact]
+        public async void TestGetGmailDb()
+        {
+            string userGmail = "test@gmail.com";
+            using var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync("https://localhost:7156/api/GetEmail", Jsonconver(userGmail));
+            TryCatch(response);
+
+
+           Assert.Equal(response.StatusCode, HttpStatusCode.OK);
+
         }
 
 
+
+        public StringContent Jsonconver(object ob)
+        {
+            return new StringContent(JsonConvert.SerializeObject(ob),
+               Encoding.UTF8,
+               "application/json");
+        }
+
+        public async Task <HttpResponseMessage> TryCatch(HttpResponseMessage? response)
+        {
+            try
+            {
+                var tt = response.EnsureSuccessStatusCode();
+                return tt;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(await response.Content.ReadAsStringAsync());
+            }
+        }
     }
 }
