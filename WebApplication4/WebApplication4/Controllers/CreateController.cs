d1538b45fe0cabf6a4603d5a1559ea8338607205
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-19 01:07:36 +0300

    created controller for DB (Add User and get Email)

---
+++ WebApplication4/WebApplication4/Controllers/CreateController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Core.Interfaces;
+using Core.Models;
+using UserService;
+using Application;
+
+namespace WebApplication4.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CreateController : ControllerBase
+    {
+        private readonly UserAddService _create;
+
+        public CreateController(UserAddService create)
+        {
+            this._create = create;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddUser([FromBody] User user)
+        {
+            try
+            {
+                if(!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                await _create.Add(user);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+    }
+}
---
+++ WebApplication4/WebApplication4/Controllers/GetEmailController.cs
@@ -0,0 +1,26 @@
+﻿using Application;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WebApplication4.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GetEmailController : Controller
+    {
+        private readonly UserReadService _userReadService;
+
+        public GetEmailController (UserReadService userReadService)
+        {
+            _userReadService = userReadService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetInfoUser([FromBody] string Email)
+        {
+            return  await _userReadService.GetInfoGmail(Email) != null  ? Ok() : NotFound("Not found Email");
+
+        }
+    }
+}
