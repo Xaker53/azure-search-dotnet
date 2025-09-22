18c7d85825d19337423733ba486667829c762629
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-23 00:21:03 +0300

    created DeleteUserController

---
+++ WebApplication4/WebApplication4/Controllers/UserController/DeleteUserController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers.UserController
+{
+    //[Route("api/[controller]")]
+    public class DeleteUserController (UserDeleteService _userDelete) : Controller
+    {
+        [HttpDelete ("api/DeleteUser/{EmailUser}")]
+        public async Task<IActionResult> DeleteUser(string EmailUser)
+        {
+            try
+            {
+                return await _userDelete.Delete(EmailUser) == true? Ok() : throw new Exception("User not found or deleted");
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ WebApplication4/WebApplication4/Controllers/UserController/GetEmailController.cs
@@ -0,0 +1,28 @@
+﻿using Application;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WebApplication4.Controllers.UserController
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GetEmailController : Controller
+    {
+        private readonly UserReadService _userReadService;
+
+        public GetEmailController (UserReadService userReadService)
+        {
+            _userReadService = userReadService;
+        }
+
+        [HttpGet]
+        public async Task<User> GetInfoUser([FromQuery] string Email)
+        {
+            //return  await _userReadService.GetInfoGmail(Email) != null  ? Ok() : NotFound("Not found Email"); // need to return User
+            return await _userReadService.GetInfoGmail(Email);
+            //return threw Exception();
+
+        }
+    }
+}
