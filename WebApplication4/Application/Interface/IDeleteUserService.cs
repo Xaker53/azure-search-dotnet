64d9a11e668643ef76a954280769276671c3e59b
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-23 00:24:02 +0300

    creaded interface for deleteuserservice

---
+++ WebApplication4/Application/Interface/IDeleteUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace Application.Interface
+{
+    internal interface IDeleteUserService
+    {
+        public Task<bool> Delete(string Gmail);
+    }
+}
