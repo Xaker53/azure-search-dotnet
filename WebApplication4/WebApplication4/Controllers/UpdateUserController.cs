03f53f93a79c403aeece2ad67bdcf9ac31ae5685
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-22 20:35:49 +0300

    added user update controller

---
+++ WebApplication4/WebApplication4/Controllers/UpdateUserController.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using Application;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UpdateUserController : Controller
+    {
+        private readonly UserUpdateService? _userUpdateService;
+
+        public UpdateUserController(UserUpdateService userUpdate)
+        {
+            _userUpdateService = userUpdate;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateInfoUser([FromBody]UserDTO changes )
+        {
+            try
+            {
+                var result = await _userUpdateService.UpdateUser(changes);
+                if (result != null)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    throw new Exception("Error updating data");
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
