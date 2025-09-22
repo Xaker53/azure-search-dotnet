7b0e025897a1186ed590087c6065f6867f713dbc
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-22 20:33:52 +0300

    add DTO

---
+++ WebApplication4/Core/Entities/UserDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Models
+{
+    [Index(nameof(UserGmail), IsUnique = true)]
+    public class UserDTO
+    {
+
+        public string? UserName { get; set; }
+
+        //[Required]
+        [Required(ErrorMessage = "Incorrect Email")]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Incorrect Email")]
+        public string? UserGmail { get; set; }
+
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Incorrect Email")]
+        public string? OtherEmail { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? IndexName { get; set; }
+
+        public string? ApiKey { get; set; }
+
+        //public ICollection<History>? histories { get; set; } = new List<History>();
+    }
+}
