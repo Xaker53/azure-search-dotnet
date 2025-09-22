caa67980699fcadcadcbc26d532654325149bd78
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-23 00:22:10 +0300

    UserDelete implementation

---
+++ WebApplication4/Persistence/Interactions/UserDelete.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Core.Models;
+using Persistence.Models;
+
+namespace Persistence.Interactions
+{
+    public class UserDelete : IDelete
+    {
+        public async Task<bool> DeleteUser(string email)
+        {
+            var EmailUser = new UserGetByGmail().GetByGmail(email);
+            if (EmailUser.Result != null)
+            {
+                using (var DbContex = new UserdbContext())
+                {
+                    DbContex.users.Remove(EmailUser.Result);
+                    await DbContex.SaveChangesAsync();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
