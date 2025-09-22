de1dca0a83a555660813dfa31dc15d891412a137
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-23 00:25:06 +0300

    implemented interface

---
+++ WebApplication4/Application/Services/UserDeleteService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interface;
+using Core.Interfaces;
+
+namespace Application.Services
+{
+    public class UserDeleteService (IDelete userDelete) : IDeleteUserService
+    {
+        public Task<bool> Delete(string Gmail)
+        {
+            return userDelete.DeleteUser(Gmail);
+        }
+    }
+}
