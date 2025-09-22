e241ef6eb7533aceb60304e7b24547abe639e884
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-09-22 20:37:02 +0300

    user service Interface and implementation

---
+++ WebApplication4/Application/Interface/IUpdateService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace Application.Interface
+{
+    public interface IUpdateService
+    {
+        public Task<User> UpdateUser(UserDTO NewChanges);
+        public string Json(User UpdatedUser);
+    }
+}
---
+++ WebApplication4/Persistence/Interactions/UserUpdate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core;
+using Core.Interfaces;
+using Core.Models;
+using Persistence.Models;
+
+namespace Persistence.Interactions
+{
+    public class UserUpdate : IUpdate
+    {
+        public async Task<User> UpdateUser(string email, PropertyInfo NewChanges, string NewSomething)
+        {
+            using (var DbContext = new UserdbContext())
+            {
+                var emailUser = new UserGetByGmail().GetByGmail(email);
+
+                if (emailUser.Result != null)
+                {
+                    foreach (var Change in typeof(User).GetProperties())
+                    {
+                        if (Change.Name == NewChanges.Name || NewChanges.Name == nameof(UserDTO.OtherEmail) && Change.Name == nameof(User.UserGmail))
+                        {
+                            Change.SetValue(emailUser.Result, NewSomething);
+                            break;
+                        }
+                    }
+                    DbContext.Update(emailUser.Result);
+                    await DbContext.SaveChangesAsync();
+                }
+                return emailUser.Result;
+            }
+        }
+    }
+}
