842c7481e1901f7e99161f3d5ff623974dfa2f07
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-10-21 16:54:38 +0300

    separate verification and hashing

---
+++ WebApplication4/Application/Interface/Auth/IPasswordVerify.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Interface.Auth
+{
+    public interface IPasswordVerify
+    {
+        //string Generate(string password, string salt);
+
+        bool Verify(string password, string HashedPassword);
+    }
+}
---
+++ WebApplication4/infrastructure/PasswordVerify.cs
@@ -0,0 +1,17 @@
+﻿
+using Application.Interface.Auth;
+using Infrastructure;
+
+namespace Infrastructure
+{
+    public class PasswordVerify : IPasswordVerify
+    {
+        public string Generate(string password, string salt) =>
+            BCrypt.Net.BCrypt.EnhancedHashPassword((password + salt));
+
+        public bool Verify(string password, string HashedPassword) => BCrypt.Net.BCrypt.EnhancedVerify(password, HashedPassword);
+
+    }
+
+}
+
