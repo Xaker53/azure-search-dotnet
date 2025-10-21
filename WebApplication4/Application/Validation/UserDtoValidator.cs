e0a4781dabebeda4e960e5a287d45ad8066a8aa5
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-10-21 16:55:32 +0300

    added validator for emptiness

---
+++ WebApplication4/Application/Validation/UserDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interface;
+using Core.Models;
+
+namespace Application.Validation
+{
+    public class UserDtoValidator : IUserDtoValidator
+    {
+
+        public bool HasOtherFieldsFilled (UserDTO Dto)
+        {
+            if (Dto == null || string.IsNullOrEmpty(Dto.UserGmail)) return false;
+
+            var hasOther = typeof(UserDTO)
+                .GetProperties()
+                .Where(p => p.Name != nameof(UserDTO.UserGmail))
+                .Any(p => p.GetValue(Dto) != null);
+            return hasOther;
+        }
+    }
+}
