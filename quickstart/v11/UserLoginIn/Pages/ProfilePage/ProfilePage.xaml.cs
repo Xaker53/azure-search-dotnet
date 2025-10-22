d86a8ab5ac7cfea2758fea67344e0b5aaf58c56e
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-10-22 23:40:41 +0300

    fead: New event button
    add OnClickCancel
    addOnClickShowPassword
    add Setup page

--- quickstart/v11/UserLoginIn/Pages/ProfilePage/ProfilePage.xaml.cs
+++ quickstart/v11/UserLoginIn/Pages/ProfilePage/ProfilePage.xaml.cs
@@ -6,4 +6,27 @@ public partial class ProfilePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	public void Setup(string Gmail, string name, string password)
+	{
+		GmailEntry.Text = Gmail;
+		NameEntry.Text = name;
+		PasswordEntry.Text = password;
+
+        EmptyUserName.Text = name;
+
+    }
+
+	private async void OnClickCancel(object? sender, EventArgs e)
+	{
+		await Navigation.PopAsync();
+        Navigation.RemovePage(this);
+
+    }
+
+	private void OnClickShowPassword (object? sender, EventArgs e)
+	{
+        PasswordEntry.IsPassword = !PasswordEntry.IsPassword;
+
+    }
 }
