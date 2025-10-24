48ac3eb189a0947cae2c4dbc95d34fc5abee2d81
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-10-24 16:30:53 +0300

    feat: Add event
    +add OnSaveChanges

--- quickstart/v11/UserLoginIn/Pages/ProfilePage/ProfilePage.xaml.cs
+++ quickstart/v11/UserLoginIn/Pages/ProfilePage/ProfilePage.xaml.cs
@@ -1,19 +1,33 @@
+
+using Azure;
+using Core.Entities.MappingProfiles;
+using Newtonsoft.Json;
+using UserLoginIn.Interface;
+
 namespace UserLoginIn;
 
 public partial class ProfilePage : ContentPage
 {
-	public ProfilePage()
+	private string _JwtToken;
+	private readonly IUpdateUserRequests _updateUserRequests;
+	private UserRequest _UpdateUser;
+    public ProfilePage(IUpdateUserRequests updateUser)
 	{
 		InitializeComponent();
+		_updateUserRequests = updateUser;
 	}
 
-	public void Setup(string Gmail, string name, string password)
+	public void Setup(UserRequest userRequest, string JwtToken)
 	{
-		GmailEntry.Placeholder = Gmail;
-		NameEntry.Placeholder = name;
-		PasswordEntry.Placeholder = password;
+        //GmailEntry.Placeholder = Gmail;
+        //NameEntry.Placeholder = name;
+        //PasswordEntry.Placeholder = password;
 
-        EmptyUserName.Text = name;
+        //      EmptyUserName.Text = name;
+        _UpdateUser = userRequest;
+        BindingContext = _UpdateUser;
+
+        _JwtToken = JwtToken;
 
     }
 
@@ -28,5 +42,38 @@ public partial class ProfilePage : ContentPage
 	{
         PasswordEntry.IsPassword = !PasswordEntry.IsPassword;
 
+    }
+
+	private async void OnSaveChanges(object? sender, EventArgs e)
+	{
+        try
+        {
+
+            _UpdateUser.OtherGmail = GmailEntry.Text != _UpdateUser.Gmail? GmailEntry.Text:null;
+            _UpdateUser.Password = PasswordEntry.Text == null? null : PasswordEntry.Text;
+
+            var response = await _updateUserRequests.FetchToServer(_UpdateUser, JwtTokenIn: _JwtToken);
+            response.EnsureSuccessStatusCode();
+            if (response != null)
+            {
+                await DisplayAlert("Server:", "Success", "OK");
+
+                if (!string.IsNullOrWhiteSpace(_UpdateUser.OtherGmail))
+                {
+                    _UpdateUser.Gmail = _UpdateUser.OtherGmail;
+                    _UpdateUser.OtherGmail = null;
+                }
+                Setup(_UpdateUser, _JwtToken);
+            }
+
+        }
+        catch (HttpRequestException ex)
+        {
+            await DisplayAlert("Server:", ex.Message, "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error:", ex.Message, "OK");
+        }
     }
 }
