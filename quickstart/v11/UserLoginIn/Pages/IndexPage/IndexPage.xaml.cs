67dee4c293afc9657603b41b9c6894cc5fbb46b6
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-10-27 20:33:39 +0200

    added IndexPage

---
+++ quickstart/v11/UserLoginIn/Pages/IndexPage/IndexPage.xaml.cs
@@ -0,0 +1,48 @@
+using ICSharpCode.SharpZipLib.Core;
+using CommunityToolkit.Maui.Storage;
+
+
+namespace UserLoginIn;
+
+public partial class IndexPage : ContentPage
+{
+
+    private string pathFile = "";
+    private string algorithm = "Rake";
+    private CancellationTokenSource tokenSource;
+
+    public IndexPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnScanAllSystemChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value)
+        {
+            
+            FilePathEntry.Text = "";
+            pathFile = "";
+        }
+    }
+
+    private void OnScanAllSystemLabelTapped(object sender, EventArgs e)
+    {
+        ScanAllSystemCheckBox.IsChecked = !ScanAllSystemCheckBox.IsChecked;
+    }
+
+    private void OnRecoverableTextChanged(object sender, CheckedChangedEventArgs e)
+    {
+        
+    }
+
+    private async void OnSelectFolderClick (object sender, EventArgs e)
+    {
+        var pick = await FolderPicker.Default.PickAsync(default);
+        if (pick.Folder != null)
+        {
+            pathFile = pick.Folder.Path;
+            FilePathEntry.Text = pathFile;
+        }
+    }
+}
