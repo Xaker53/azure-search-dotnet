6eb6d1bfda7e2d99797cd62978c8f8f4fa4b3230
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-10-28 22:18:38 +0200

    feat: Implemented events
    +OnStartIndex
    +OnDelete
    +OnStop

--- quickstart/v11/UserLoginIn/Pages/IndexPage/IndexPage.xaml.cs
+++ quickstart/v11/UserLoginIn/Pages/IndexPage/IndexPage.xaml.cs
@@ -1,5 +1,10 @@
-using ICSharpCode.SharpZipLib.Core;
+
+using Aspose.Words.XAttr;
+using AzureSearch.Quickstart;
+using CommunityToolkit.Maui.Core.Primitives;
 using CommunityToolkit.Maui.Storage;
+using ICSharpCode.SharpZipLib.Core;
+
 
 
 namespace UserLoginIn;
@@ -7,14 +12,17 @@ namespace UserLoginIn;
 public partial class IndexPage : ContentPage
 {
 
-    private string pathFile = "";
-    private string algorithm = "Rake";
+    private string _pathFile = "";
+    private string _algorithm = "Rake";
     private CancellationTokenSource tokenSource;
+    private Program program;
+
 
     public IndexPage()
 	{
 		InitializeComponent();
-        AlgorithmPicker.SelectedItem = algorithm;
+        AlgorithmPicker.SelectedItem = _algorithm;
+        program = new();
 
     }
 
@@ -23,7 +31,7 @@ public partial class IndexPage : ContentPage
         if (e.Value)
         {
             FilePathEntry.Text = "";
-            pathFile = "";
+            _pathFile = "";
         }
     }
 
@@ -42,9 +50,55 @@ public partial class IndexPage : ContentPage
         var pick = await FolderPicker.Default.PickAsync(default);
         if (pick.Folder != null)
         {
-            pathFile = pick.Folder.Path;
-            FilePathEntry.Text = pathFile;
+            _pathFile = pick.Folder.Path;
+            FilePathEntry.Text = _pathFile;
             ScanAllSystemCheckBox.IsChecked = false;
+        }
+    }
+
+
+    private async void OnStartIndex (object sender, EventArgs e)
+    {
+        if (ScanAllSystemCheckBox.IsChecked ||File.Exists(FilePathEntry.Text) ||Directory.Exists(FilePathEntry.Text))
+        {
+            //IsBusy = true;
+            _algorithm = AlgorithmPicker.SelectedItem.ToString();
+            tokenSource = new();
+            ButtonStartIndex.IsEnabled = false;
+            ButtonStartIndex.BackgroundColor = Colors.Gray;
+            await Task.Run(() =>
+            {
+                tokenSource.Token.ThrowIfCancellationRequested();
+                program.UploadDocuments(_pathFile, _algorithm, RecoverableTextCheckBox.IsChecked);
+
+            }, tokenSource.Token);
+            ButtonStartIndex.IsEnabled = true;
+            ButtonStartIndex.BackgroundColor = Color.Parse("#6366F1");
+            await DisplayAlert("Succes", "Indexing is complete", "OK");
+        }
+        else
+        {
+            await DisplayAlert("Notes", "You need to select a folder or scan the entire system", "OK");
+        }
+    }
+
+    private async void OnDelete (object sender, EventArgs e)
+    {
+        bool dialogResult = await DisplayAlert("Delete all index?", "Delete", "Yes","No");
+        if (dialogResult)
+        {
+            program.RecreateIndex();
+            await DisplayAlert("Succes", "Indexing is delete", "OK");
+        }
+    }
+
+    private async void OnStop (object sender, EventArgs e)
+    {
+        if (tokenSource != null)
+        {
+            program.CancelToken();
+            tokenSource?.Cancel();
+            await DisplayAlert("Succes", "Indexing is stop", "OK");
         }
     }
 }
