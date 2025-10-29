d1f342c0abe67fb8edc1beea15679e25a0145ab8
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-10-29 20:44:26 +0200

    implemented GlobalState

---
+++ quickstart/v11/UserLoginIn/Tools/GlobalState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities.MappingProfiles;
+using Core.Models;
+public class GlobalState 
+{ 
+    public string JwtToken { get; set; }
+    public UserRequest CurrentUser { get; set; } 
+}
