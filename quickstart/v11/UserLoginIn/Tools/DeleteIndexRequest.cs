17b1158c58be438ac16e6892836d286f747ce390
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-11-01 17:49:10 +0200

    first commit

---
+++ quickstart/v11/UserLoginIn/Tools/DeleteIndexRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using UserLoginIn.Interface;
+
+namespace UserLoginIn.Tools
+{
+    sealed class DeleteIndexRequest : IDeleteIndexRequest
+    {
+        private readonly string _url;
+        private readonly ITryDelete _tryDelete;
+
+        public DeleteIndexRequest(IOptions<ApiSettings> UrlService, ITryDelete tryDelete)
+        {
+            _url = UrlService.Value.DeleeteUserIndexUrl;
+            _tryDelete = tryDelete;
+        }
+
+        public async Task<HttpResponseMessage> TryCatch(string UserEmail, string JwtToken = "")
+        {
+             return await _tryDelete.DeleteRequest(UserEmail, JwtToken, _url);
+        }
+    }
+}
---
+++ quickstart/v11/UserLoginIn/Tools/TryDelete.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure;
+using UserLoginIn.Interface;
+
+namespace UserLoginIn.Tools
+{
+    sealed class TryDelete : ITryDelete
+    {
+        private HttpResponseMessage response;
+        public async Task<HttpResponseMessage> DeleteRequest (string UserEmail, string JwtToken = "", string _url = "")
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", $"{JwtToken}");
+                    response = await httpClient.DeleteAsync($"{_url}{UserEmail}");
+                    return response;
+                }
+                catch (Exception e)
+                {
+                    //MessageBox.Show("Error.");
+                    return response;
+                }
+            }
+        }
+    }
+}
