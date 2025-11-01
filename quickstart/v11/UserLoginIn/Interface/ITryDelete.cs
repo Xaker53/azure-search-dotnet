c66b45536d8da59669c60fcc2ceb834cdcab5d7b
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-11-01 17:47:26 +0200

    implement an interface for deletion

---
+++ quickstart/v11/UserLoginIn/Interface/ITryDelete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLoginIn.Interface
+{
+    public interface ITryDelete
+    {
+        public Task<HttpResponseMessage> DeleteRequest(string UserEmail, string JwtToken = "", string _url = "");
+    }
+}
