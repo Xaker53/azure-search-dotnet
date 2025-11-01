80aa143b5ec3690f085316a2c1323893072b3026
Andrii53 <Laggerfeedlag1992@gmail.com>
2025-11-01 17:48:35 +0200

    implemented delete index user

---
+++ WebApplication4/Application/CQRS/UserDeleteIndex/UserDeleteIndexHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interface;
+using Core.Interfaces;
+using MediatR;
+using MediatR.Pipeline;
+
+namespace Application.CQRS.UserDeleteIndex
+{
+    public class UserDeleteIndexHandler : IRequestHandler<UserDeleteIndexCQRS, bool>
+    {
+        private readonly IRead _userRead;
+        private readonly IConnectAzure _connectAzure;
+        public UserDeleteIndexHandler(IRead read, IConnectAzure connectAzure)
+        {
+            _userRead = read;
+            _connectAzure = connectAzure;
+        }
+        public async Task<bool> Handle(UserDeleteIndexCQRS request, CancellationToken cancellationToken)
+        {
+            var result = await _userRead.GetByGmail(request.UserGmail);
+            if (result != null)
+            {
+                await _connectAzure.DeleteAllByUserIdAsync(result.UserId.ToString());
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ WebApplication4/WebApplication4/Controllers/UserController/DeletingUserIndexes.cs
@@ -0,0 +1,29 @@
+﻿using Application.CQRS.DeleteUser;
+using Application.CQRS.UserDeleteIndex;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers.UserController
+{
+
+    //[Authorize(Policy = "ReadPolicy")]
+
+    public class DeletingUserIndexesController (ISender _sender) : Controller
+    {
+        [HttpDelete ("api/DeleteIndex/{UserGmail}")]
+        public async Task<IActionResult> DeleteIndex(UserDeleteIndexCQRS Email)
+        {
+            try
+            {
+                return await _sender.Send(Email) == true ? Ok() : throw new Exception("User not found");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            
+        }
+    }
+}
